acb5887ccae0c64122c36e4be4cc26774f411637
spaila <spaila@HIN-DRWP2N3-LT>
2024-09-13 14:15:47 +0530

    Add project files.

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SiteInfomatiomnDetails> SiteInfomatiomnDetails { get; set; }
+        public DbSet<Siteinfo> Siteinfo { get; set; }
+
+    }
+}
